2b94ffd6bbece20ee6442010736d0d53b0dbe7dd
joram <mwemajoram254@gmail.com>
2026-08-08 00:21:46 +0300

    feat:Handle websocket messages

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,16 +13,41 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
+	//Handle websocket messages and keep the connection alive
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
+				log.Println("websocket read error:", err)
+			}
+			break
+		}
+		//Handle the message and send the response back to the client
+		if messageType == websocket.TextMessage {
+			textMessage := string(message)
+			response := "Received message: " + textMessage
+			if err := conn.WriteMessage(websocket.TextMessage, []byte(response)); err != nil {
+				log.Println("websocket write error:", err)
+				break
+			}
+		} else if messageType == websocket.CloseMessage {
+			break
+		}
+	}
+}
+
+func upgrade(r *http.Request, w http.ResponseWriter) (any, any) {
+	panic("unimplemented")
 }
 
 func main() {
 	//Initialize the api Gateway
-	apigateway := openapi.NewAPIGateway()
+	apiGateway := openapi.NewAPIGateway()
 
 	//Define routes
 	routes := []Route{
 		{"POST", "/users", "/services/user-service",
-			func(w http.Response, r *http.Reques) {
+			func(w http.ResponseWriter, r *http.Request) {
 				apiGateway.proxyUserService(r, w)
 			},
 		},
